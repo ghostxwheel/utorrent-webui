01e8f884ac75aec931d273e17191bca2bb41b6ed
Grigoriy Sergeev <ghostxwheel@gmail.com>
2015-06-27 20:14:20 +0300

    Changes to server.js and added package.json

--- utorrent/server.js
+++ utorrent/server.js
@@ -1,14 +1,14 @@
 var express = require("express"),
-    httpProxy = require("http-proxy"),
-    proxy = httpProxy.createProxyServer({}),
+    http = require("http"),
     app = express(),
     compression = require("compression"),
-    open = require("open"),
+    //open = require("open"),
     serveIndex = require("serve-index"),
-    port = 8081,
+    port = process.env.OPENSHIFT_NODEJS_PORT|| 8081,
+	host = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1',
     publicPath = "/app",
     directory = __dirname,
-    launchUrl = "http://localhost:" + port + publicPath,
+    launchUrl = "http://" + host + ":" + port + publicPath,
     year = 60 * 60 * 24 * 365 * 1000;
     
 // use compress middleware to gzip content
@@ -21,8 +21,8 @@ express.static.mime.default_type = "text/xml";
 app.use(publicPath, express.static(directory, { maxAge: year, hidden: true }));
 
 // enable directory listing
-app.use("/app", serveIndex(__dirname, {"icons": true}));
-app.use("/gui", function(req,res) {
+app.use("/", serveIndex(__dirname, {"icons": true}));
+app.use("/gui", function(req, res) {
     for(var prop in req.headers) {
         if(req.headers.hasOwnProperty(prop)) {
             if(prop !== "authorization" && prop !== "cookie") {
@@ -31,14 +31,29 @@ app.use("/gui", function(req,res) {
         }
     }
     
-    proxy.web(req, res, { target: "http://localhost:9002/gui" + req.url.toString() });
+	http.request({
+		host: "grifonpc.ddns.net",
+		port: 9002,
+		path: "/gui" + req.url.toString(),
+		headers: {
+			"Authorization" : req.headers.authorization,
+			"Cookie" : req.headers.cookie
+		}
+	}, function(innerRes) {
+		innerRes.on('data', function (data) {
+			res.write(data);
+			res.end();
+		});
+	});
+	
+    //proxy.web(req, res, { target: "http://grifonpc.ddns.net:9002/gui" + req.url.toString() });
 });
 
 // start server
-app.listen(port);
+app.listen(port, host);
 
 // launch uri in default browser
-open(launchUrl);
+//open(launchUrl);
 
 // log to server console
 console.log("OpenUI5 SDK server running at\n  => " + launchUrl + " \nCTRL + C to shutdown")
